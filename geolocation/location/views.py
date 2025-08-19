336d171c317459415e1f9d9a34586aeb2d479b61
dinamitrii <k_karaivanov@abv.bg>
2025-08-19 07:06:18 +0300

    and so on on request

--- geolocation/location/views.py
+++ geolocation/location/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import requests
+import json
+
 
 # Create your views here.
+def index(request):
+    res = requests.get('http://ip-api.com/json/24.48.0.1')
+    text_data = res.text
+    json_data = json.loads(text_data)
+
+    return render(request, 'index.html', {'data':json_data})
