ce49fb319f9a6e0ebb8ca8ec6209a6221c1fff01
dinamitrii <k_karaivanov@abv.bg>
2025-08-19 09:34:56 +0300

    added "static" directory

--- geolocation/location/urls.py
+++ geolocation/location/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+
+import weather
 from location import views
 from weather import get_current_weather
 
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('', views.get_current_weather(get_current_weather()), name='weather'),
+
+    path('', weather.get_current_weather(), name='weather'),
 ]
