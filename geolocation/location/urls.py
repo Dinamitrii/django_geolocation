37049c34b035db6eccabe9a0296ccba10c4383a9
dinamitrii <k_karaivanov@abv.bg>
2025-08-19 08:54:54 +0300

    GG

--- geolocation/location/urls.py
+++ geolocation/location/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from location import views
+from weather import get_current_weather
+
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('', views.get_current_weather(get_current_weather()), name='weather'),
 ]
--- geolocation/location/views.py
+++ geolocation/location/views.py
@@ -1,6 +1,15 @@
+from dotenv import load_dotenv
+import segno
+import os
 from django.shortcuts import render
 import requests
 import json
+
+from tornado.escape import json_decode
+
+from location.qr_code_generator import link_to_qr_code
+
+load_dotenv()
 
 
 # Create your views here.
@@ -10,8 +19,34 @@ def index(request):
     ip_data = json.loads(ip.text)
 
     ###
-    res = requests.get('http://ip-api.com/json/',{'data':ip_data})
+    res = requests.get('http://ip-api.com/json/', {'data': ip_data})
     text_data = res.text
     json_data = json.loads(text_data)
     ###
-    return render(request, 'index.html', {'data':json_data})
+
+    return render(request, 'index.html', {'data': json_data})
+
+
+def get_current_weather(request_url):
+    ###
+    ip = requests.get('https://api.ipify.org?format=json')
+    ip_data = json.loads(ip.text)
+
+    ###
+    res = requests.get('http://ip-api.com/json/', {'data': ip_data})
+    text_data = res.text
+    json_data = json.loads(text_data)
+    ###
+
+    request_url = (f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={json_data['city']}&lang=bg'
+                   f'&units=metric')
+    weather_data = requests.get(request_url).json()
+
+
+    return render(weather_data, 'weather.html', {'data': weather_data}, )
+
+
+# def link_to_qr_code(link):
+#     # link make to qr here
+#     qr_code = segno.make(link)
+#     qr_code.save('static/images/qr_code.png', scale=4)
