1bb9583a1015f762edb3ef746e68334f251aa8bd
dinamitrii <k_karaivanov@abv.bg>
2025-08-19 07:11:34 +0300

    gg

--- geolocation/location/views.py
+++ geolocation/location/views.py
@@ -5,8 +5,13 @@ import json
 
 # Create your views here.
 def index(request):
+    ###
+    ip = request.META.get('REMOTE_ADDR')
+    ip_data = requests.get('https://api.ipify.org')
+    ip_data_dict = json.loads(ip_data.text)
+    ###
     res = requests.get('http://ip-api.com/json/24.48.0.1')
     text_data = res.text
     json_data = json.loads(text_data)
-
+    ###
     return render(request, 'index.html', {'data':json_data})
